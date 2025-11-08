6f082a15e26b1d8ef56ca4820d62b4cc2231877e
𝐀𝐤𝐚𝐬𝐡 𝐃𝐚𝐤𝐬𝐡𝐰𝐚𝐧𝐬𝐡𝐢 : ZoxxOP <akp954834@gmail.com>
2025-11-08 06:28:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ZoxxOP/AnanyaxMusic",
+    "https://github.com/ZoxxOP/akash",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
